3f8843504a34f6957589cf25c4c95d36097f3f78
Konrad Beck <konrad.beck@merchantcapital.co.za>
2025-07-18 17:20:32 +0200

    Refactor simple_test.py for improved readability by formatting headers and data structure.

--- simple_test.py
+++ simple_test.py
@@ -3,10 +3,15 @@ import requests
 
 # Check what methods are allowed first
 token = "19351ba078375ba46b49de4deedcbba0e2399acb"
-headers = {'Authorization': f'Token {token}', 'Content-Type': 'application/json'}
+headers = {"Authorization": f"Token {token}", "Content-Type": "application/json"}
 
-url_with_params = "http://localhost:8000/api/loan-accounts/?unique_fields=account_number"
-data = [{"account_number": "string3", "business": 1, "status": "Active"}, {"account_number": "string2", "business": 1, "status": "Active"}]
+url_with_params = (
+    "http://localhost:8000/api/loan-accounts/?unique_fields=account_number"
+)
+data = [
+    {"account_number": "string3", "business": 1, "status": "Active"},
+    {"account_number": "string2", "business": 1, "status": "Active"},
+]
 response = requests.patch(url_with_params, headers=headers, json=data)
 
 print(f"PATCH Upsert Status: {response.status_code}")
