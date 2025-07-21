f455043dbb045c6ff0a806f5e2f2aa9b7e012d7d
Konrad Beck <konrad.beck@merchantcapital.co.za>
2025-07-21 14:35:57 +0200

    Update loan_account_test.py with new token and URL, and modify test data for PATCH request

--- loan_account_test.py
+++ loan_account_test.py
@@ -2,15 +2,15 @@
 import requests
 
 # Check what methods are allowed first
-token = "534978d245de72cbeacd6b924b294962c3dc4eb4"
+token = "0718bce74f66b18547b54059a7a1081133ac3b7d"
 headers = {"Authorization": f"Token {token}", "Content-Type": "application/json"}
 
 url_with_params = (
-    "http://localhost:8000/api/loan-accounts/?unique_fields=account_number"
+    "https://sit-sg.augend.io/api/loan-accounts/?unique_fields=account_number"
 )
 data = [
-    {"account_number": "string3", "business": 1},
-    {"account_number": "string2", "business": 1},
+    {"account_number": "string3", "business": 4},
+    {"account_number": "string2", "business": 5},
 ]
 response = requests.patch(url_with_params, headers=headers, json=data)
 
